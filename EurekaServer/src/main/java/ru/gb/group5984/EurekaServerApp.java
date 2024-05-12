b42be9696150f350a2a8032ec765c6ac0e0f5dfa
alex123 <alex123@dot.com>
2024-05-12 14:36:59 +0500

    midsave 2024 05 12 14.30

--- EurekaServer/src/main/java/ru/gb/group5984/EurekaServerApp.java
+++ EurekaServer/src/main/java/ru/gb/group5984/EurekaServerApp.java
@@ -17,4 +17,5 @@ public class EurekaServerApp {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApp.class, args);
     }
+
 }
--- configserver/src/main/java/ru/gb/group5984/ConfigServerApp.java
+++ configserver/src/main/java/ru/gb/group5984/ConfigServerApp.java
@@ -15,4 +15,5 @@ public class ConfigServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApp.class, args);
     }
+
 }

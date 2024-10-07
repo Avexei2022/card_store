64c6d30397ba58127362ec5767502136cce69a48
alex123 <alex123@dot.com>
2024-10-07 11:49:09 +0500

    fix ignore

--- store_resource_server/src/main/java/ru/gb/group5984/service/db/ServerDbServiceImpl.java
+++ store_resource_server/src/main/java/ru/gb/group5984/service/db/ServerDbServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import ru.gb.group5984.aspect.TrackUserAction;
 import ru.gb.group5984.model.basket.Basket;
